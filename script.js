288e59a8c3c7f1a7998aff0bb22eb1e4c0cc3155
Tawhid Maruf <85786025+mrftech@users.noreply.github.com>
2023-05-06 19:25:21 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const clockElement = document.querySelector('.clock');
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+
+    clockElement.textContent= currentTime;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initialize the clock immediately
+updateClock();
